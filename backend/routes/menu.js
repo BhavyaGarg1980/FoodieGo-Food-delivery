042652651b385e81cf7efecd3cfafbaaf0f7922f
[Bhavya Garg] <[ldhbhavya@gmail.com]>
2026-03-11 12:48:18 +0530

    changed

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,32 +1,23 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
 const router = express.Router();
-
 const menuFile = path.join(__dirname, "../menu.json");
-
 
 // GET menu by restaurant
 router.get("/:restaurantId", (req, res) => {
-
   const data = fs.readFileSync(menuFile);
   const menu = JSON.parse(data);
 
   const restaurantMenu = menu.filter(
     item => item.restaurantId == req.params.restaurantId
   );
-
   res.json(restaurantMenu);
-
 });
-
 
 // ADD menu item
 router.post("/", (req, res) => {
-
   const { restaurantId, name, price, image } = req.body;
-
   const data = fs.readFileSync(menuFile);
   let menu = JSON.parse(data);
 
@@ -39,11 +30,8 @@ router.post("/", (req, res) => {
   };
 
   menu.push(newItem);
-
   fs.writeFileSync(menuFile, JSON.stringify(menu, null, 2));
-
   res.json({ message: "Menu item added", item: newItem });
-
 });
 
 module.exports = router;
--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
 const router = express.Router();
-
 const restaurantsFile = path.join(__dirname, "../restaurants.json");
-
 router.post("/", (req, res) => {
-
   const { name, description, image } = req.body;
-
   let restaurants = [];
 
   // read existing data
@@ -26,11 +21,8 @@ router.post("/", (req, res) => {
   };
 
   restaurants.push(newRestaurant);
-
   fs.writeFileSync(restaurantsFile, JSON.stringify(restaurants, null, 2));
-
   res.json({ message: "Restaurant added" });
-
 });
 
 module.exports = router;
